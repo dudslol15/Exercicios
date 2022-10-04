20edba52af0e3864386bbfd44e0e96dcfa1dd420
EDUARDO NIENKOTTER MARCELINO <eduardo_n_marcelino@estudante.sesisenai.org.br>
2022-10-04 20:17:14 -0300

    Exercicios 1 e 2

---
+++ exercicio1.js
@@ -0,0 +1,5 @@
+//Imprimir números de 0 a 100
+
+for (let num = 0; num <= 100; num++){
+    console.log(num)
+  }
---
+++ exercicio2.js
@@ -0,0 +1,8 @@
+//2 números e imprimir todos entre eles
+
+let n1 = Number(prompt("Número 1: "))
+let n2 = Number(prompt("Número 2: "))
+
+for (let num = n1; num < n2; num++) {
+  console.log(num)
+}
