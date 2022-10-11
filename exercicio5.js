d5a84a4b4890969b2aeaca66e360bdc40d85538c
EDUARDO NIENKOTTER MARCELINO <eduardo_n_marcelino@estudante.sesisenai.org.br>
2022-10-10 22:22:42 -0300

    Exercicio 5 foi feito

--- exercicio5.js
+++ exercicio5.js
@@ -1,89 +1,32 @@
-let carros = []
+let carrosVendidos = [[], [], [], [], [], [], [], [], [], [], [], []]
 
-let mes = prompt("Mês de vendas:")
-let qtd = prompt("Quantidade de vendas:")
+for (let numeroMes = 0; numeroMes < 12; numeroMes++) {
+  while (true){
+    let nomeCarro = prompt(`Digite o nome do proximo carro vendido no mes ${numeroMes + 1}.
+     Digite 'fim' para pular para o proximo mes.`)
 
-if (mes === "janeiro") {
-  for (let num = 1; num <= qtd; num++) {
-    var carro = prompt(`Carro ${num}:`)
-    carros.push(carro)
+    if (nomeCarro.toUpperCase() === `FIM`) {
+      break
+    } else carrosVendidos[numeroMes].push(nomeCarro)
   }
-  console.log(carros)
-}
-if (mes === "fevereiro") {
-  for (let num = 1; num <= qtd; num++) {
-    var carro = prompt(`Carro ${num}:`)
-    carros.push(carro)
+  let mes
+  switch (numeroMes){
+    case 0: mes = "janeiro"; break
+    case 1: mes = "fevereiro"; break
+    case 2: mes = "março"; break
+    case 3: mes = "abril"; break
+    case 4: mes = "maio"; break
+    case 5: mes = "junho"; break
+    case 6: mes = "julho"; break
+    case 7: mes = "agosto"; break
+    case 8: mes = "setembro"; break
+    case 9: mes = "outubro"; break
+    case 10: mes = "novembro"; break
+    case 11: mes = "dezembro"; break
   }
-  console.log(carros)
-}
-if (mes === "março") {
-  for (let num = 1; num <= qtd; num++) {
-    var carro = prompt(`Carro ${num}:`)
-    carros.push(carro)
-  }
-  console.log(carros)
-}
-if (mes === "abril") {
-  for (let num = 1; num <= qtd; num++) {
-    var carro = prompt(`Carro ${num}:`)
-    carros.push(carro)
-  }
-  console.log(carros)
-}
-if (mes === "maio") {
-  for (let num = 1; num <= qtd; num++) {
-    var carro = prompt(`Carro ${num}:`)
-    carros.push(carro)
-  }
-  console.log(carros)
-}
-if (mes === "junho") {
-  for (let num = 1; num <= qtd; num++) {
-    var carro = prompt(`Carro ${num}:`)
-    carros.push(carro)
-  }
-  console.log(carros)
-}
-if (mes === "julho") {
-  for (let num = 1; num <= qtd; num++) {
-    var carro = prompt(`Carro ${num}:`)
-    carros.push(carro)
-  }
-  console.log(carros)
-}
-if (mes === "agosto") {
-  for (let num = 1; num <= qtd; num++) {
-    var carro = prompt(`Carro ${num}:`)
-    carros.push(carro)
-  }
-  console.log(carros)
-}
-if (mes === "setembro") {
-  for (let num = 1; num <= qtd; num++) {
-    var carro = prompt(`Carro ${num}:`)
-    carros.push(carro)
-  }
-  console.log(carros)
-}
-if (mes === "outubro") {
-  for (let num = 1; num <= qtd; num++) {
-    var carro = prompt(`Carro ${num}:`)
-    carros.push(carro)
-  }
-  console.log(carros)
-}
-if (mes === "novembro") {
-  for (let num = 1; num <= qtd; num++) {
-    var carro = prompt(`Carro ${num}:`)
-    carros.push(carro)
-  }
-  console.log(carros)
-}
-if (mes === "dezembro") {
-  for (let num = 1; num <= qtd; num++) {
-    var carro = prompt(`Carro ${num}:`)
-    carros.push(carro)
-  }
-  console.log(carros)
+
+  console.log(`Os carros vendidos no mês de ${mes} são:`)
+
+  for (let indCarro = 0; carrosVendidos[numeroMes][indCarro] !== undefined; indCarro++)
+    console.log(`${carrosVendidos[numeroMes][indCarro]}`)
 }
