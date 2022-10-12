0da91aaa8f36f3bfbd4cd404ee1af5356b85a4d9
EDUARDO NIENKOTTER MARCELINO <eduardo_n_marcelino@estudante.sesisenai.org.br>
2022-10-11 22:20:25 -0300

    Foi feito os exercicios 6 e 8

---
+++ exercicio7.js
@@ -0,0 +1,78 @@
+let carrosVendidos = [[], [], [], [], [], [], [], [], [], [], [], []]
+
+for (let numeroMes = 0; numeroMes < 12; numeroMes++) {
+  while (true) {
+    let nomeCarro = prompt(`Digite o carro vendido no mes ${numeroMes + 1}.
+     Digite 'fim' para pular para o proximo mes.`)
+
+    if (nomeCarro.toUpperCase() === `FIM`) {
+      break
+    } else carrosVendidos[numeroMes].push(nomeCarro)
+  }
+  let mes
+  switch (numeroMes) {
+    case 0: mes = "janeiro"; break
+    case 1: mes = "fevereiro"; break
+    case 2: mes = "março"; break
+    case 3: mes = "abril"; break
+    case 4: mes = "maio"; break
+    case 5: mes = "junho"; break
+    case 6: mes = "julho"; break
+    case 7: mes = "agosto"; break
+    case 8: mes = "setembro"; break
+    case 9: mes = "outubro"; break
+    case 10: mes = "novembro"; break
+    case 11: mes = "dezembro"; break
+  }
+
+  console.log(`Os carros vendidos no mês de ${mes} são:`)
+
+  for (let indCarro = 0; carrosVendidos[numeroMes][indCarro] !== undefined; indCarro++)
+    console.log(`${carrosVendidos[numeroMes][indCarro]}`)
+}
+
+let totalCarrosPorMes = [0,0,0,0,0,0,0,0,0,0,0,0]
+
+/*for (let contador = 0; carrosVendidos[0][contador] !== undefined; contador++){
+  totalCarrosPorMes[0] = contador + 1
+}
+for (let contador = 0; carrosVendidos[1][contador] !== undefined; contador++) {
+  totalCarrosPorMes[1] = contador + 1
+}
+for (let contador = 0; carrosVendidos[2][contador] !== undefined; contador++) {
+  totalCarrosPorMes[2] = contador + 1
+}*/
+
+totalCarrosPorMes = [
+  carrosVendidos[0].lenght,
+  carrosVendidos[1].lenght,
+  carrosVendidos[2].lenght,
+  carrosVendidos[3].lenght,
+  carrosVendidos[4].lenght,
+  carrosVendidos[5].lenght,
+  carrosVendidos[6].lenght,
+  carrosVendidos[7].lenght,
+  carrosVendidos[8].lenght,
+  carrosVendidos[9].lenght,
+  carrosVendidos[10].lenght,
+  carrosVendidos[11].lenght
+]
+
+console.log(`O total de carros vendidos em cada mês é: ${totalCarrosPorMes.join(", ")}`)
+
+//Quantidade por tipo de carro vendido
+let nomeCarro = prompt("Digite o nome de um carro para saber a quantidade total de carros vendidos")
+let nCarrosVendidos = 0
+
+for (let numeroMes = 0; numeroMes < 12; numeroMes++){
+
+  for (let indCarro = 0; carrosVendidos[numeroMes][indCarro] !== undefined; indCarro++){
+    if (carrosVendidos[numeroMes][indCarro] === nomeCarro) nCarrosVendidos++
+  }
+}
+
+console.log(`O total de carros vendidos do tipo ${nomeCarro} é: ${nCarrosVendidos}`)
+
+//Imprimir o 3
+//let a = ['a', [], [], 12, true, [1, 2, [1, 2, 3]]]
+//console.log(a[5][2][2])
